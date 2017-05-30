d94bd5cb71e7bb8dd534c5ad174fb0e48d5b8da0
Sylvain Hallé <shalle@acm.org>
2017-05-29 20:18:57 -0400

    Corrected empty lab to follow library refactorings

--- Source/src/MyLaboratory.java
+++ Source/src/MyLaboratory.java
@@ -2,12 +2,15 @@ import ca.uqac.lif.labpal.Laboratory;
 
 public class MyLaboratory extends Laboratory
 {
+	@Override
+	public void setup()
+	{
+		// Write your setup code here
+	}
 	
 	public static void main(String[] args)
 	{
 		// Nothing else to do here
 		MyLaboratory.initialize(args, MyLaboratory.class);
 	}
-	
-	
 }
